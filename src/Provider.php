c5875b948db693eadc0a31d6aa97ac43f6055ffc
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-06-04 10:06:06 +0800

    ready for start dev

--- src/Provider.php
+++ src/Provider.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace MultipleChain\Tron;
 
+use IEXBase\TronAPI\Tron;
 use MultipleChain\Enums\ErrorType;
+use IEXBase\TronAPI\Provider\HttpProvider;
+use IEXBase\TronAPI\Exception\TronException;
 use MultipleChain\Interfaces\ProviderInterface;
 use MultipleChain\BaseNetworkConfig as NetworkConfig;
 
@@ -14,6 +17,48 @@ class Provider implements ProviderInterface
      * @var NetworkConfig
      */
     public NetworkConfig $network;
+
+    /**
+     * @var array<string,array<string>>
+     */
+    public array $nodes = [
+        'mainnet' => [
+            'id' => '0x2b6653dc',
+            'node' => 'mainnet',
+            'name' => 'TronGrid Mainnet',
+            'host' => 'https://api.trongrid.io',
+            'event' => 'https://api.trongrid.io',
+            'explorer' => 'https://tronscan.org/'
+        ],
+        'testnet' => [
+            'id' => '0xcd8690dc',
+            'node' => 'testnet',
+            'name' => 'TronGrid Nile Testnet',
+            'host' => 'https://nile.trongrid.io',
+            'event' => 'https://event.nileex.io',
+            'explorer' => 'https://nile.tronscan.org/'
+        ]
+    ];
+
+    /**
+     * @var array<string>
+     */
+    public array $node;
+
+    /**
+     * @var Tron
+     */
+    public Tron $tron;
+
+    /**
+     * @var bool
+     */
+    public bool $connectionFailed = false;
+
+    /**
+     * @var string
+     */
+    public string $connectionError = '';
 
     /**
      * @var Provider|null
@@ -59,6 +104,19 @@ class Provider implements ProviderInterface
     {
         self::$instance = $this;
         $this->network = new NetworkConfig($network);
+        $this->node = $this->nodes[$this->network->isTestnet() ? 'testnet' : 'mainnet'];
+        $this->node['host'] = $this->network->getRpcUrl() ?? $this->node['host'];
+        $this->node['event'] = $this->network->getWsUrl() ?? $this->node['event'];
+        try {
+            $this->tron = new Tron(
+                new HttpProvider($this->node['host']),
+                new HttpProvider($this->node['host']),
+                new HttpProvider($this->node['event'])
+            );
+        } catch (TronException $e) {
+            $this->connectionFailed = true;
+            $this->connectionError = $e->getMessage();
+        }
     }
 
     /**
@@ -75,7 +133,15 @@ class Provider implements ProviderInterface
      */
     public function checkRpcConnection(?string $url = null): bool
     {
-        return true;
+        try {
+            if ($this->connectionFailed) {
+                return false;
+            }
+
+            return $this->tron->getManager()->fullNode()->isConnected();
+        } catch (\Throwable $th) {
+            return false;
+        }
     }
 
     /**
@@ -84,6 +150,14 @@ class Provider implements ProviderInterface
      */
     public function checkWsConnection(?string $url = null): bool
     {
-        return true;
+        try {
+            if ($this->connectionFailed) {
+                return false;
+            }
+
+            return $this->tron->getManager()->eventServer()->isConnected();
+        } catch (\Throwable $th) {
+            return false;
+        }
     }
 }
